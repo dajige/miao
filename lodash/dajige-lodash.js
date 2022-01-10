8cbbf96fbe205e48b6bb0b21d57a45a698d34c95
dajige <1624490717@qq.com>
2022-01-10 16:16:33 +0800

    1.10

--- lodash/dajige-lodash.js
+++ lodash/dajige-lodash.js
@@ -1,3 +1,16 @@
 var dajige = {
-  chunk: function (array, size) {},
+  chunk: function (array, size) {
+    var rns = [], temp = [],n = array.length
+    for (var i = 0; i < n; i++){
+      temp.push(array[i])
+      if (temp.length == size) {
+        rns.push(temp)
+        temp = []
+      }
+    }
+    if (temp.length != 0) {
+      rns.push(temp)
+    }
+    return rns
+  },
 }
