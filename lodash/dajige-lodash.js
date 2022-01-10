f7cdb8ca75119bb7021bf8c95cef5a0877c5dadc
dajige <1624490717@qq.com>
2022-01-10 19:15:06 +0800

    1.10

--- lodash/dajige-lodash.js
+++ lodash/dajige-lodash.js
@@ -1,7 +1,9 @@
 var dajige = {
   chunk: function (array, size) {
-    var rns = [], temp = [],n = array.length
-    for (var i = 0; i < n; i++){
+    var rns = [],
+      temp = [],
+      n = array.length
+    for (var i = 0; i < n; i++) {
       temp.push(array[i])
       if (temp.length == size) {
         rns.push(temp)
@@ -10,6 +12,29 @@ var dajige = {
     }
     if (temp.length != 0) {
       rns.push(temp)
+    }
+    return rns
+  },
+  compact: function (array) {
+    var rns = []
+    for (var i = 0; i < array.length; i++) {
+      if (array[i]) {
+        rns.push(array[i])
+      }
+    }
+    return rns
+  },
+  drop: function (array, number = 1) {
+    if (number == 0) {
+      return array
+    }
+    if (array.length < number) {
+      return []
+    }
+    let n = array.length
+    let rns = []
+    for (let i = number; i < n; i++) {
+      rns.push(array[i])
     }
     return rns
   },
